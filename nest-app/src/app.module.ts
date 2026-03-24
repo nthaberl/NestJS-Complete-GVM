94957c187dc5ea04bba4a4873248cde1d74201a0
Natascha Haberl <nt.haberl@gmail.com>
2026-03-24 12:50:52 -0700

    module 2 complete

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DemoModule } from './demo/demo.module';
+import { ServiceGeneratedController } from './service-generated/service-generated.controller';
+import { ServiceGeneratedService } from './service-generated/service-generated.service';
 
 @Module({
   imports: [DemoModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ServiceGeneratedController],
+  providers: [AppService, ServiceGeneratedService],
 })
 export class AppModule {}
